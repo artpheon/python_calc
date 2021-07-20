0a7db02a4655a63cb510bd342cec18db9e277694
Artur N <idcdtokms@gmail.com>
2021-07-20 12:24:49 +0300

    Added tests

--- calc.py
+++ calc.py
@@ -1,9 +1,17 @@
-from math import sqrt
+from math import *
 
-
-class Calculator():
+class Calculator(object):
     def __init__(self) -> None:
-        pass
+        self.operations = [
+            self.sum,
+            self.sub,
+            self.mult,
+            self.div,
+            self.mod,
+            self.power,
+            self.squareRoot,
+            self.logarithm,
+            self.sine]
 
     def sum(self, a, b):
         return a + b
@@ -30,10 +38,18 @@ class Calculator():
         return a ** b
     
     def squareRoot(self, a):
-        return sqrt(a)
+        if a < 0:
+            raise ValueError('Cannot find sqrt for negative number')
+        else:
+            return sqrt(a)
     
     def logarithm(self, num, base):
-        return 1 + self.logarithm(num / base, base) if (num > base - 1) else 0
+        if num < 0:
+            raise ValueError('Cannot find log for negative number')
+        if base <= 0:
+            raise ValueError('Base is negative or zero')
+        else:
+            return 1.0 + self.logarithm(num // base, base) if (num > base - 1) else 0
 
 # Bhaskara I's sine approximation formula.
     def sine(self, x):
@@ -42,3 +58,5 @@ class Calculator():
         else:
             inter = 180 - x
             return (4 * x * inter) / (40500 - x * inter)
+    
+    #listing
--- start.py
+++ start.py
@@ -1,22 +1,48 @@
-from .Calculator import Calculator
+import math
+from calc import Calculator
 
 def testCalculator(a, b):
-    c = Calculator
-    print('Sum: ' + c.sum(a, b))
-    print('Sub: ' + c.sum(a, b))
-    print('Mult: ' + c.sum(a, b))
+    c = Calculator()
+    print('_____________________\nTest for nums', a, b)
+    print('Sum: ', c.sum(a, b), "and", a + b)
+    print('Sub: ', c.sub(a, b), 'and', a - b)
+    print('Mult: ', c.mult(a, b), 'and', a * b)
     try:
-        print('Div: ' + c.sum(a, b))
-    except ValueError:
-        print('No division by zero!')
+        print('Div: ', c.div(a, b), 'and', a / b)
+    except ValueError as e:
+        print('Div:', e.args[0])
     try:
-        print('Mod: ' + c.sum(a, b))
-    except ValueError:
-        print('No division by zero!')
-    print('Pow: ' + c.sum(a, b))
-    print('Sqrt: ' + c.sum(a, b))
-    print('Log: ' + c.sum(a, b))
+        print('Mod: ', c.mod(a, b), 'and', math.modf(a))
+    except ValueError as e:
+        print('Mod:', e.args[0])
+    print('Pow: ', c.power(a, b), 'and', math.pow(a, b))
+    try:
+        print('Sqrt: ', c.squareRoot(a), 'and', math.sqrt(a))
+    except ValueError as e:
+        print('Sqrt:', e.args[0])
+    try:
+        print('Log: ', c.logarithm(a, b), 'and', math.log(a, b))
+    except ValueError as e:
+        print('Log:', e.args[0])
+    try:
+        print('Sin: ', c.sine(a), 'and', math.sin(a))
+    except ValueError as e:
+        print('Sin:', e.args[0])
     
 
-def start(a, b):
-    
+def runTests():
+    testCalculator(-1, 3)
+    testCalculator(0, 0)
+    testCalculator(5, -2)
+    testCalculator(6, 0)
+    testCalculator(6, 6)
+    testCalculator(6, 6)
+    testCalculator(30, 6)
+
+def promptCalculator():
+    c = Calculator()
+    operation = input('Enter the operation:\n0 - add\n1 - subtract\n2 - multiply\n3 - divide\n4 - find modulo\n5 - raise in power\n6 - find square root\n7 - find logarithm\n8 - find sine of angle(in degrees)')
+    try:
+        c.operations[operation]
+    except:
+        pass
